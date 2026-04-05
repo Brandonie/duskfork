TakaRikka/dusk

initial commit with working 3D view at all times

src/d/d_meter_map.cpp
@@ -620,7 +620,13 @@ void dMeterMap_c::draw() {
         #endif
         mMapJ2DPicture->setAlpha(alpha);
 
+        #if TARGET_PC
+        mMapJ2DPicture->draw(mDoGph_gInf_c::ScaleHUDXLeft(tmp2), tmp3, sizeX, sizeY, false, false,
+                             false);
+        #else
         mMapJ2DPicture->draw(tmp2, tmp3, sizeX, sizeY, false, false, false);
+        #endif
+
         mMapJ2DPicture->calcMtx();
     }
 }

Brandonie/duskfork

src/d/d_meter_map.cpp
@@ -628,7 +628,13 @@ void dMeterMap_c::draw() {
         #endif
         mMapJ2DPicture->setAlpha(alpha);
 
+        #if TARGET_PC
+        mMapJ2DPicture->draw(mDoGph_gInf_c::ScaleHUDXLeft(drawPosX), drawPosY, sizeX, sizeY, false, false,
+                             false);
+        #else
         mMapJ2DPicture->draw(drawPosX, drawPosY, sizeX, sizeY, false, false, false);
+        #endif
+
         mMapJ2DPicture->calcMtx();
     }
 }